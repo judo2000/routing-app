dfdaaabe5a6d855791fcc71e626d000f84aa200f
Stephen Moore <judo2000@comcast.net>
2024-09-23 15:51:26 -0600

    Add styling and dummy data with actual links from news page

--- app/news/[article]/page.js
+++
@@ -1,8 +0,0 @@
-export default function ArticlePage({ params }) {
-  return (
-    <>
-      <h1>Article Page</h1>
-      <p>Article Id: {params.article}</p>
-    </>
-  );
-}
--- app/news/page.js
+++ app/news/page.js
@@ -1,19 +1,23 @@
 import Link from "next/link";
 
+import { DUMMY_NEWS } from "@/dummy-news";
+
 export default function NewsPage() {
   return (
     <>
       <h1>News Page</h1>
-      <ul>
-        <li>
-          <Link href="/news/article-1">Article 1</Link>
-        </li>
-        <li>
-          <Link href="/news/article-2">Article 2</Link>
-        </li>
-        <li>
-          <Link href="/news/article-3">Article 3</Link>
-        </li>
+      <ul className="news-list">
+        {DUMMY_NEWS.map((newsItem) => (
+          <li key={newsItem.id}>
+            <Link href={`/news/${newsItem.slug}`}>
+              <img
+                src={`/images/news/${newsItem.image}`}
+                alt={newsItem.title}
+              />
+              <span>{newsItem.title}</span>
+            </Link>
+          </li>
+        ))}
       </ul>
     </>
   );
