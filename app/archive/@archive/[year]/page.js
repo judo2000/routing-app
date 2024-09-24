e1aa97f40bf81a7e0ee617fe170f6a18f861c918
Stephen Moore <judo2000@comcast.net>
2024-09-24 15:35:20 -0600

    Configure catch all and fallback routes

--- app/archive/@archive/[year]/page.js
+++
@@ -1,8 +0,0 @@
-import NewsList from "@/comonents/news-list";
-import { getNewsForYear } from "@/lib/news";
-
-export default function FilteredNewsPage({ params }) {
-  const newsYear = params.year;
-  const news = getNewsForYear(newsYear);
-  return <NewsList news={news} />;
-}
